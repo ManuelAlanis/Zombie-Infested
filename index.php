1223ef39ad5067799e4badd1c5b6b4d932dc7df0
JMurilloM <murillo.jorge@uabc.edu.mx>
2014-11-05 18:44:42 -0800

    comprar boletos

--- index.php
+++ index.php
@@ -424,7 +424,7 @@ if ($name1Err =="" && $email1Err=="" && $apellido1Err =="" && $telefono1Err ==""
   <center><h3>INSCRIBIRME</h3></center>
   </div>
   <div id="boton" href="https://zombieinfested.boletia.com/">
-	<center><h2 style="color:white">COMPRAR BOLETOS</h2></center>  
+	<center><h2 style="color:white" href="https://zombieinfested.boletia.com/">COMPRAR BOLETOS</h2></center>  
   </div>
   </div>
   
